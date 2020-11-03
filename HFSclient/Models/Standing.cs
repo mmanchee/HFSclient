9afc5c59a04f34da7bc9b07f10835af12482285c
Mike Manchee <mikemanchee@gmail.com>
2020-11-03 10:29:55 -0800

    add Home Index info
    Co-authored-by: Joseph Nilles <jbnilles24@gmail.com>
    Co-authored-by: Jeff Dinsmore <hello@jeffdinsmore.com>
    Co-authored-by: Daniel Schaaf <daniel.schaaf@outlook.com>

--- HFSclient/Models/Standing.cs
+++ HFSclient/Models/Standing.cs
@@ -3,7 +3,7 @@ namespace HFSclient.Models
   public class Standing
   {
     public int StandingId { get; set; }
-    public virtual ApplicationUser User { get; set; }
+    //public virtual ApplicationUser User { get; set; }
     public int GroupId { get; set; }
     public int Wins { get; set; }
     public int Ties { get; set; }
--- HFSclient/Startup.cs
+++ HFSclient/Startup.cs
@@ -9,6 +9,9 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using HFSclient.Models;
 
 namespace HFSclient
 {
@@ -33,6 +36,23 @@ namespace HFSclient
 
 
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+      services.AddEntityFrameworkMySql()
+        .AddDbContext<HFSclientContext>(options => options
+        .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
+    
+      services.AddIdentity<ApplicationUser, IdentityRole>()
+        .AddEntityFrameworkStores<HFSclientContext>()
+        .AddDefaultTokenProviders();
+
+      services.Configure<IdentityOptions>(options =>
+      {
+        options.Password.RequireDigit = false;
+        options.Password.RequiredLength = 0;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequiredUniqueChars = 0;
+      });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
