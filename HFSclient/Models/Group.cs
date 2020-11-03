b1071ad5050d4b078ca751847f9e5b3eadff5376
Mike Manchee <mikemanchee@gmail.com>
2020-11-03 13:18:27 -0800

    add Header and Footer
    Co-authored-by: Joseph Nilles <jbnilles24@gmail.com>
    Co-authored-by: Jeff Dinsmore <hello@jeffdinsmore.com>
    Co-authored-by: Daniel Schaaf <daniel.schaaf@outlook.com>

--- HFSclient/Models/Group.cs
+++ HFSclient/Models/Group.cs
@@ -5,6 +5,7 @@ namespace HFSclient.Models
     public int GroupId { get; set; }
     public int LeagueId { get; set; }
     public virtual ApplicationUser User { get; set; }
+    public string TeamName { get; set; }
     public int LeagueSeason { get; set; }
     public string OwnerRole { get; set; }
     public virtual League League { get; set; }
