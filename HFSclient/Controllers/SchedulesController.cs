9c41377d60c4f885b732be4425bda9a8212be949
jbnilles <jbnilles24@gmail.com>
2020-11-04 11:05:02 -0800

    add schedule generator
    Co-authored-by: Daniel Schaaf <daniel.schaaf@outlook.com>

--- HFSclient/Controllers/SchedulesController.cs
+++ HFSclient/Controllers/SchedulesController.cs
@@ -91,22 +91,22 @@ namespace HFSclient.Controllers
 
     [Authorize(Roles = "Administrator")]
     [HttpPost]
-    public async Task<ActionResult> Sim(int GroupId1, int GroupId2, int Week ) //might be schedule id
+    public async Task<ActionResult> Sim(Schedule schedule ) //might be schedule id
     {
-      Schedule schedule = new Schedule(); //likely be changed
-      schedule.ScheduleId = _db.Schedules.Count() + 1; //likely be changed
-      if(GroupId1 != 0)
-      {
-        schedule.GroupId1 = GroupId1;
-      }
-      if(GroupId2 != 0)
-      {
-        schedule.GroupId2 = GroupId2;
-      }
-      if(Week != 0)
-      {
-        schedule.Week = Week;
-      }
+      // Schedule schedule = new Schedule(); //likely be changed
+      // schedule.ScheduleId = _db.Schedules.Count() + 1; //likely be changed
+      // if(GroupId1 != 0)
+      // {
+      //   schedule.GroupId1 = GroupId1;
+      // }
+      // if(GroupId2 != 0)
+      // {
+      //   schedule.GroupId2 = GroupId2;
+      // }
+      // if(Week != 0)
+      // {
+      //   schedule.Week = Week;
+      // }
       List<Roster> roster1 = _db.Rosters.Where(x => x.GroupId == schedule.GroupId1).ToList();
       List<Roster> roster2 = _db.Rosters.Where(x => x.GroupId == schedule.GroupId2).ToList();
       int teamscore = 0;
@@ -115,7 +115,7 @@ namespace HFSclient.Controllers
           Game game = await Game.GetGameByWeek(schedule.Week, roster.PlayerId); //get game by roster.GameId;
           Tracker tracker = new Tracker();
           tracker.GameId = game.GameId;
-          tracker.GroupId = GroupId1;
+          tracker.GroupId = schedule.GroupId1;
           tracker.Position = roster.Position;
           tracker.Points = game.CalcScore(); //calculate fantasy points here 
           if(tracker.Position != "bench")//make sure not a bench player and add to schedule points for the team
@@ -132,7 +132,7 @@ namespace HFSclient.Controllers
           Game game = await Game.GetGameByWeek(schedule.Week, roster.PlayerId); //get game by roster.GameId;
           Tracker tracker = new Tracker();
           tracker.GameId = game.GameId;
-          tracker.GroupId = GroupId1;
+          tracker.GroupId = schedule.GroupId2;
           tracker.Position = roster.Position;
           tracker.Points = game.CalcScore(); //calculate fantasy points here 
           if(tracker.Position != "bench")//make sure not a bench player and add to schedule points for the team
@@ -144,10 +144,142 @@ namespace HFSclient.Controllers
           
       }
       schedule.Team2Score = teamscore;
+      //update standings
+
+      Standing standingTeam1 = _db.Standings.Where(x => x.GroupId == schedule.GroupId1).FirstOrDefault();
+      Standing standingTeam2 = _db.Standings.Where(x => x.GroupId == schedule.GroupId2).FirstOrDefault();
+
+      if(schedule.Team1Score > schedule.Team2Score)
+      {
+        standingTeam1.Wins++;
+        standingTeam2.Losses++;
+        standingTeam1.PtsFor += schedule.Team1Score;
+        standingTeam2.PtsFor += schedule.Team2Score;
+        standingTeam1.PtsAgst += schedule.Team2Score;
+        standingTeam1.PtsAgst += schedule.Team1Score;
+        
+      }
+      else if(schedule.Team1Score < schedule.Team2Score)
+      {
+        standingTeam2.Wins++;
+        standingTeam1.Losses++;
+        standingTeam2.PtsFor += schedule.Team2Score;
+        standingTeam1.PtsFor += schedule.Team1Score;
+        standingTeam2.PtsAgst += schedule.Team1Score;
+        standingTeam1.PtsAgst += schedule.Team2Score;
+        
+      }
+      else
+      {
+        standingTeam2.Ties++;
+        standingTeam1.Ties++;
+        standingTeam2.PtsFor += schedule.Team2Score;
+        standingTeam1.PtsFor += schedule.Team1Score;
+        standingTeam2.PtsAgst += schedule.Team1Score;
+        standingTeam1.PtsAgst += schedule.Team2Score;
+      }
+
+
+
+
+
+      _db.Entry(standingTeam1).State = EntityState.Modified; 
+      _db.Entry(standingTeam2).State = EntityState.Modified;
+
+
       _db.Schedules.Add(schedule);
       _db.SaveChanges();
       return RedirectToAction("Index"); //enter proper view / next sim
     }
+
+    [Authorize(Roles = "Administrator")]// 12345678  1   2   3  4  5 6   7  8
+                                        // 1         12  13 14 15 16 17 18  12
+                                        //2     12  24 25 26 27 28 23  12
+                                        // 3    34  31 
+    public ActionResult BuildSchedule(int LeagueId)                                         
+    {
+      List<Group> temp = _db.Groups.Where(x => x.LeagueId == LeagueId).ToList();
+      List<int> teams = new List<int>();
+      foreach (Group group in temp)
+      {
+          teams.Append(group.GroupId);
+      }
+      //let teams = ["1",'2','3','4','5','6','7','8','9']
+      int count = teams.Count();
+      if( count % 2 == 1)
+      {
+        count++;
+        teams.Append(0);
+      }
+      
+      List<int> teams1 = teams.GetRange(0,count/2);
+      // let teams1 = ["1",'2','3','4','5'];//5742
+      // let teams2 = ['6','7','8','9',''];
+
+      List<int> teams2 =teams.GetRange(count/2, count/2);
+
+      
+      // if(count % 2 == 1)
+      // {
+      // count++;
+      // teams2.push('');
+      // }
+      for(int i = 0 ; i < 12; i++)
+      {
+          // console.log(teams1)
+          // console.log(teams2)
+          // console.log('')
+        for(int j = 0; j < count / 2; j++)
+        {
+          //console.log(teams1[j] + teams2[j]);//create game
+          Schedule schedule =new Schedule();
+          schedule.GroupId1 = teams1[j];
+          schedule.GroupId2 = teams2[j];
+          schedule.IsComplete = false;
+          schedule.Week = i + 1;
+          _db.Schedules.Add(schedule);
+          
+        }
+        
+        if(i % (count/2 - 1) == 0 && i != 0)
+        {
+          int tmp = teams1[count/2 - 1];
+          teams1.RemoveAt(count/2 - 1);
+
+          teams2.Add(tmp);
+         
+          tmp = teams2[0];
+          teams2.RemoveAt(0);
+          int temp2 = teams1[0];
+          teams1.RemoveAt(0);
+          
+          teams1.Prepend(tmp);
+          teams1.Prepend(temp2);
+       
+        }
+        else
+        {
+          int tmp = teams1[count/2 - 1];
+          teams1.RemoveAt(count/2 - 1);
+          //let temp = teams1.pop();
+          
+          teams2.Add(tmp);
+          
+          tmp = teams2[0];
+          teams2.RemoveAt(0);
+                    
+          teams1.Prepend(tmp);
+        
+          
+          // temp = teams2.shift();
+          // teams1.unshift(temp)
+          
+        }
+      }
+      _db.SaveChanges();
+      return RedirectToAction("index");// need to change to proper view
+    }
+
     
   }
 }
